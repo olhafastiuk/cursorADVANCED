a09115622c227c1f2d57afb1dbbe262c5748c761
Olha Fastiuk <olhafastiuk@gmail.com>
2021-06-02 01:27:37 +0300

    HW #1: Change currency

--- hw-1/script.js
+++ hw-1/script.js
@@ -7,15 +7,15 @@ function truncated(num) {
     truncated(3.1416);
 
 let MaxNumber = Math.max(lipstick, eyeshadows, perfumes);
-console.log('Max price: ' + MaxNumber + 'UAH');
+console.log('Max price: ' + MaxNumber + 'USD');
 let MinNumber = Math.min(lipstick, eyeshadows, perfumes);
-console.log('Min price: ' + MinNumber + 'UAH');
+console.log('Min price: ' + MinNumber + 'USD');
 let sum = lipstick + eyeshadows + perfumes;
-console.log('Amount: ' + sum + 'UAH');
+console.log('Amount: ' + sum + 'USD');
 let sumRounded = Math.floor(lipstick) + Math.floor(eyeshadows) + Math.floor(perfumes);
-console.log('Rounded amount: ' + sumRounded + 'UAH');
+console.log('Rounded amount: ' + sumRounded + 'USD');
 let sumRounded_100 = Math.trunc(sum/100) * 100;
-console.log('Rounded amount to 100: ' + sumRounded_100 + 'UAH');
+console.log('Rounded amount to 100: ' + sumRounded_100 + 'USD');
 let sumBool;
     if (sumRounded % 2 === 0) {
         sumBool = 'even number';
@@ -25,37 +25,37 @@ let sumBool;
 console.log('is the sum an even number? : ' + sumBool);
 
 let remainder_500 = 500 - sum;
-console.log('Delivery from 500 UAH: ' + remainder_500 + 'UAH');
+console.log('Delivery from 500 USD: ' + remainder_500 + 'USD');
 let medium = (truncated(lipstick) + truncated(eyeshadows) + truncated(perfumes)) / 3;
-console.log('Average price: ' + medium + 'UAH');
+console.log('Average price: ' + medium + 'USD');
 let discount = Math.random() * 100;
 console.log('Discount: ' + truncated(discount) + '%');
 let sumDiscount = truncated( sum - sum * discount / 100);
-console.log('Amount due: ' + sumDiscount + 'UAH');
+console.log('Amount due: ' + sumDiscount + 'USD');
 let profit = truncated(sumDiscount - sum/2);
-console.log('Profit: ' + profit + 'UAH');
+console.log('Profit: ' + profit + 'USD');
 
 document.querySelector(".receipt").innerHTML = `
     <h1>RECEIPT</h1>
     <div class="line"></div>
     <div class="info">
-        <p>Max price: <span>${MaxNumber} UAH</span></p>
-        <p>Min price: <span>${MinNumber} UAH</span></p>
-        <p>Amount: <span>${sum} UAH</span></p>
-        <p>Rounded amount: <span>${sumRounded} UAH</span></p>
-        <p>Rounded amount to 100: <span>${sumRounded_100} UAH</span></p>
-        <p>Avarange price: <span>${medium} UAH</span></p>
+        <p>Max price: <span>${MaxNumber} USD</span></p>
+        <p>Min price: <span>${MinNumber} USD</span></p>
+        <p>Amount: <span>${sum} USD</span></p>
+        <p>Rounded amount: <span>${sumRounded} USD</span></p>
+        <p>Rounded amount to 100: <span>${sumRounded_100} USD</span></p>
+        <p>Avarange price: <span>${medium} USD</span></p>
         <p>Is the sum an even number? <span>${sumBool}</span></p>
     </div>
     <div class="line"></div>
      <div class="check">
-        <p>Delivery from 500 UAH: <span>${remainder_500} UAH</span></p>
+        <p>Delivery from 500 USD: <span>${remainder_500} USD</span></p>
         <p>Discount: <span>${truncated(discount)}%</span> </p>
-        <p>Amount due: <span>${sumDiscount} UAH</span></p>
+        <p>Amount due: <span>${sumDiscount} USD</span></p>
     </div>
     <div class="line"></div>
     <div class="check">
-        <p>Profit: <span>${profit} UAH</span></p>
+        <p>Profit: <span>${profit} USD</span></p>
     </div>`;
 
 // Максимальнальна ціна: ${MaxNumber}грн; <br>

74c5436b0cc5a36d527f79120bd1497efac04db7
Olha Fastiuk <olhafastiuk@gmail.com>
2021-06-01 23:37:52 +0300

    HW #1:Add receipt calculation script

---
+++ hw-1/script.js
@@ -0,0 +1,49 @@
+const lipstick = 15.678;
+const eyeshadows = 90.2345;
+const perfumes = 123.965;
+function truncated(num) {
+    return Math.trunc(num * 100) / 100;
+    }
+    truncated(3.1416);
+
+let MaxNumber = Math.max(lipstick, eyeshadows, perfumes);
+console.log('Max price: ' + MaxNumber + 'UAH');
+let MinNumber = Math.min(lipstick, eyeshadows, perfumes);
+console.log('Min price: ' + MinNumber + 'UAH');
+let sum = lipstick + eyeshadows + perfumes;
+console.log('Amount: ' + sum + 'UAH');
+let sumRounded = Math.floor(lipstick) + Math.floor(eyeshadows) + Math.floor(perfumes);
+console.log('Rounded amount: ' + sumRounded + 'UAH');
+let sumRounded_100 = Math.trunc(sum/100) * 100;
+console.log('Rounded amount to 100: ' + sumRounded_100 + 'UAH');
+let sumBool;
+    if (sumRounded % 2 === 0) {
+        sumBool = 'even number';
+    } else {
+        sumBool = 'odd number';
+    }
+console.log('is the sum an even number? : ' + sumBool);
+
+let remainder_500 = 500 - sum;
+console.log('Delivery from 500 UAH: ' + remainder_500 + 'UAH');
+let medium = (truncated(lipstick) + truncated(eyeshadows) + truncated(perfumes)) / 3;
+console.log('Average price: ' + medium + 'UAH');
+let discount = Math.random() * 100;
+console.log('Discount: ' + discount + '%');
+let sumDiscount = truncated( sum - sum * discount / 100);
+console.log('Amount due: ' + sumDiscount + 'UAH');
+let profit = truncated(sumDiscount - sum/2);
+console.log('Profit: ' + profit + 'UAH');
+
+document.querySelector(".receipt").innerHTML = `
+Максимальнальна ціна: ${MaxNumber}грн; <br>
+Мінімальна ціна: ${MinNumber}грн;<br>
+Вартість всіх товарів: ${sum}грн;<br>
+Округлена вартість всіх товарів: ${sumRounded}грн;<br>
+Округлена вартість всіх товарів до 100: ${sumRounded_100}грн;<br>
+Парне чи непарне? ${sumBool};<br>
+Здача від 500грн: ${remainder_500}грн;<br>
+Середня вартість товарів: ${medium}грн;<br>
+Ваша знижка: ${discount}%;<br>
+Сума зі знижкою: ${sumDiscount}грн;<br>
+Прибуток: ${profit}грн`;

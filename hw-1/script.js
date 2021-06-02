74280a7ecc4993f44ddc85abdd7d768cf632cb26
Olha Fastiuk <olhafastiuk@gmail.com>
2021-06-02 10:40:23 +0300

    HW #1: Fix code

--- hw-1/script.js
+++ hw-1/script.js
@@ -1,65 +1,76 @@
 const lipstick = 15.678;
 const eyeshadows = 90.2345;
 const perfumes = 123.965;
+
 function truncated(num) {
     return Math.trunc(num * 100) / 100;
     }
-    truncated(3.1416);
 
-let MaxNumber = Math.max(lipstick, eyeshadows, perfumes);
-console.log('Max price: ' + MaxNumber + 'USD');
-let MinNumber = Math.min(lipstick, eyeshadows, perfumes);
-console.log('Min price: ' + MinNumber + 'USD');
+let maxNumber = Math.max(lipstick, eyeshadows, perfumes);
+console.log('Max price: ' + maxNumber + 'USD');
+
+let minNumber = Math.min(lipstick, eyeshadows, perfumes);
+console.log('Min price: ' + minNumber + 'USD');
+
 let sum = lipstick + eyeshadows + perfumes;
 console.log('Amount: ' + sum + 'USD');
+
 let sumRounded = Math.floor(lipstick) + Math.floor(eyeshadows) + Math.floor(perfumes);
 console.log('Rounded amount: ' + sumRounded + 'USD');
-let sumRounded_100 = Math.trunc(sum/100) * 100;
+
+let sumRounded_100 = (sum/100).toFixed(0) * 100;
 console.log('Rounded amount to 100: ' + sumRounded_100 + 'USD');
-let sumBool;
+
+let isSumEven;
     if (sumRounded % 2 === 0) {
-        sumBool = 'even number';
+        isSumEven = 'even number';
     } else {
-        sumBool = 'odd number';
+        isSumEven = 'odd number';
     }
-console.log('is the sum an even number? : ' + sumBool);
+console.log('is the sum an even number? : ' + isSumEven);
+// console.log(sumRounded % 2 ===0);
 
 let remainder_500 = 500 - sum;
 console.log('Delivery from 500 USD: ' + remainder_500 + 'USD');
+
 let medium = (truncated(lipstick) + truncated(eyeshadows) + truncated(perfumes)) / 3;
 console.log('Average price: ' + medium + 'USD');
+
 let discount = Math.random() * 100;
-console.log('Discount: ' + truncated(discount) + '%');
-let sumDiscount = truncated( sum - sum * discount / 100);
-console.log('Amount due: ' + sumDiscount + 'USD');
-let profit = truncated(sumDiscount - sum/2);
-console.log('Profit: ' + profit + 'USD');
+console.log('Discount: ' + discount.toFixed(2) + '%');
+
+let sumDiscount = sum - sum * discount / 100;
+console.log('Amount due: ' + sumDiscount.toFixed(2) + 'USD');
+
+let profit = sumDiscount - sum/2;
+console.log('Profit: ' + profit.toFixed(2) + 'USD');
+
 
 document.querySelector(".receipt").innerHTML = `
     <h1>RECEIPT</h1>
     <div class="line"></div>
     <div class="info">
-        <p>Max price: <span>${MaxNumber} USD</span></p>
-        <p>Min price: <span>${MinNumber} USD</span></p>
+        <p>Max price: <span>${maxNumber} USD</span></p>
+        <p>Min price: <span>${minNumber} USD</span></p>
         <p>Amount: <span>${sum} USD</span></p>
         <p>Rounded amount: <span>${sumRounded} USD</span></p>
         <p>Rounded amount to 100: <span>${sumRounded_100} USD</span></p>
         <p>Avarange price: <span>${medium} USD</span></p>
-        <p>Is the sum an even number? <span>${sumBool}</span></p>
+        <p>Is the sum an even number? <span>${isSumEven}</span></p>
     </div>
     <div class="line"></div>
      <div class="check">
         <p>Delivery from 500 USD: <span>${remainder_500} USD</span></p>
-        <p>Discount: <span>${truncated(discount)}%</span> </p>
-        <p>Amount due: <span>${sumDiscount} USD</span></p>
+        <p>Discount: <span>${discount.toFixed(2)}%</span> </p>
+        <p>Amount due: <span>${sumDiscount.toFixed(2)} USD</span></p>
     </div>
     <div class="line"></div>
     <div class="check">
-        <p>Profit: <span>${profit} USD</span></p>
+        <p>Profit: <span>${profit.toFixed(2)} USD</span></p>
     </div>`;
 
-// Максимальнальна ціна: ${MaxNumber}грн; <br>
-// Мінімальна ціна: ${MinNumber}грн;<br>
+// Максимальнальна ціна: ${maxNumber}грн; <br>
+// Мінімальна ціна: ${minNumber}грн;<br>
 // Вартість всіх товарів: ${sum}грн;<br>
 // Округлена вартість всіх товарів: ${sumRounded}грн;<br>
 // Округлена вартість всіх товарів до 100: ${sumRounded_100}грн;<br>

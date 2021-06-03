9f35f5cec418e433d2614dcff517410168cf6946
Olha Fastiuk <olhafastiuk@gmail.com>
2021-06-04 01:41:52 +0300

    HW #2: Fix counting

--- hw-2/script.js
+++ hw-2/script.js
@@ -1,34 +1,32 @@
-function res(num1, num2){
-    let sum = 0;
-    let even = document.getElementById('even');
-    for (let i = num1;  i <= num2; i++){
-        if(even.checked){
-            if (i % 2 == 0){
-                continue
-            }
+function calculateSum(firstNumber, secondNumber, oddOnly)
+{
+    let sum = null;
+    let num1 = Math.min(firstNumber, secondNumber);
+    let num2 = Math.max(firstNumber, secondNumber);
+
+    for (let i = num1;  i <= num2; i++) {
+        if (oddOnly.checked && (i % 2 == 0)) {
+            continue
         }
-        sum = sum+ i;
+        sum = sum + i;
     }
-    if (sum==0){
-        document.querySelector(".result").innerHTML = `<p>Суму не вдалося визначити</p>`;
-    } else document.querySelector(".result").innerHTML = `<p> Сума чисел від ${num1} до ${num2}: <p class="sum"> ${sum}</p>`;
+
     return sum;
 }
 
-document.forms.publish.onsubmit = function() {
-    let number1 = Number(document.getElementById('number1').value);
-    let number2 = +document.getElementById('number2').value;
-    console.log(number1, number2);
-    console.log(res(number1, number2));
+document.forms.publish.onsubmit = function()
+{
+    const number1 = Number(document.getElementById('number1').value);
+    const number2 = Number(document.getElementById('number2').value);
+    const oddOnly = document.getElementById('checkbox');
+    let resultContainer = document.querySelector(".result");
+    let sum = calculateSum(number1, number2, oddOnly);
+    
+    if (sum === null) {
+        resultContainer.innerHTML = `<p>Суму не вдалося визначити</p>`;
+    } else {
+        resultContainer.innerHTML = `<p> Сума чисел в заданому проміжку: <p class="sum"> ${sum}</p>`;
+    }
+    
     return false;
-};
-// do{
-//     number1 = Number(prompt("Введіть перше число"))
-// }while(!(number1===Math.round(number1)));
-
-// do{
-//     number2 = Number(prompt("Введіть друге число"))
-// }while(!(number2===Math.round(number2)));
-// let even = confirm('Чи пропускати парні числа?')
-
-
+}   

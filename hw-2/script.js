45b78c54f4f7fbef53c778f99055d9dae8dd9992
Olha Fastiuk <olhafastiuk@gmail.com>
2021-06-03 23:44:10 +0300

    HW #2: Update counting script.

--- hw-2/script.js
+++ hw-2/script.js
@@ -1,24 +1,34 @@
-let number1;
-let number2;
-let sum = 0;
-do{
-    number1 = Number(prompt("Введіть перше число"))
-}while(!(number1===Math.round(number1)));
-
-do{
-    number2 = Number(prompt("Введіть друге число"))
-}while(!(number2===Math.round(number2)));
-let even = confirm('Чи пропускати парні числа?')
-
-
-for (let i = number1;  i <= number2; i++){
-    if(even){
-        if (i % 2 == 0){
-            continue
+function res(num1, num2){
+    let sum = 0;
+    let even = document.getElementById('even');
+    for (let i = num1;  i <= num2; i++){
+        if(even.checked){
+            if (i % 2 == 0){
+                continue
+            }
         }
+        sum = sum+ i;
     }
-    sum = sum+ i;
+    if (sum==0){
+        document.writeln("Суму не вдалося визначити")
+    } else document.writeln(sum);
+    return sum;
 }
-if (sum==0){
-    document.writeln("Суму не вдалося визначити")
-} else document.writeln(sum);
+
+document.forms.publish.onsubmit = function() {
+    let number1 = Number(document.getElementById('number1').value);
+    let number2 = +document.getElementById('number2').value;
+    console.log(number1, number2);
+    console.log(res(number1, number2));
+    return false;
+};
+// do{
+//     number1 = Number(prompt("Введіть перше число"))
+// }while(!(number1===Math.round(number1)));
+
+// do{
+//     number2 = Number(prompt("Введіть друге число"))
+// }while(!(number2===Math.round(number2)));
+// let even = confirm('Чи пропускати парні числа?')
+
+

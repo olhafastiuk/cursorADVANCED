c97bfbeb1e482baca8c839cf31fa0859344635af
Olha Fastiuk <olhafastiuk@gmail.com>
2021-06-14 10:41:20 +0300

    HW #3: Fix bugs

--- hw-3/script.js
+++ hw-3/script.js
@@ -2,17 +2,17 @@
 // Приклади: 1236 -> 6, 987 -> 9, 385 -> 8
 function getMaxNumber(inputNumber) {
     let arrInput = [];
-    let len = inputNumber.length;
+    const len = inputNumber.length;
     
-    for (i = 0; i < len; ++i) {
+    for (let i = 0; i < len; ++i) {
         arrInput.push(inputNumber[i]);
     }
 
     let maxNumber = arrInput[0];
 
-    for (i = 0; i <len; i++){
+    for (let i = 0; i <len; i++) {
 
-        if (arrInput[i] > maxNumber){
+        if (arrInput[i] > maxNumber) {
             maxNumber = arrInput[i];
         } 
     }
@@ -20,8 +20,10 @@ function getMaxNumber(inputNumber) {
     return maxNumber
 }
 document.forms.form_1.onsubmit = function(){
-    let inputNumber = document.getElementById('input-1').value;
-    document.getElementById("output-1").innerHTML = `Найбільше цифра у введеному числі: ${getMaxNumber(inputNumber)}`;
+    const inputNumber = document.getElementById('input-1').value;
+    const maxNumber = getMaxNumber(inputNumber);
+
+    document.getElementById("output-1").innerHTML = `Найбільше цифра у введеному числі: ${maxNumber}`;
     return false;
 }
 
@@ -30,114 +32,117 @@ document.forms.form_1.onsubmit = function(){
 function getNumberInPower(number, power) {
     let newNumber = number;
     if(power > 0){
-        for (i = 1;i < power;i++ ){
+        for (let i = 1; i < power; i++ ) {
         newNumber = newNumber * number;
         }
     } 
     if (power < 0){
         power = power * -1;
         newNumber = 1/number;
-        for (i = 1; i < power; i++){
+        for (let i = 1; i < power; i++) {
             newNumber = newNumber / number;
         }
     }
-    if (power == 0){
+    if (power === 0) {
         newNumber = 1;
     }
     return newNumber
 }
 document.forms.form_2.onsubmit = function(){
-    let number = Number(document.getElementById('input-2_1').value);
-    let power = Number(document.getElementById('input-2_2').value);
+    const number = Number(document.getElementById('input-2_1').value);
+    const power = Number(document.getElementById('input-2_2').value);
+    const numberInPower = getNumberInPower(number, power);
 
-    document.getElementById("output-2").innerHTML = `Результат: ${getNumberInPower(number, power)}`;
+    document.getElementById("output-2").innerHTML = `Результат: ${numberInPower}`;
     return false;
 }
 
 // 3.Створити функцію, яка форматує ім'я, роблячи першу букву великою. ("влад" -> "Влад", "вЛАД" -> "Влад" і так далі);
 
-function capitalize(str){
-    str = str.toLowerCase();
-    strNew = str[0].toUpperCase();
+function capitalize(name) {
+    const str = name.toLowerCase();
+    let strNew = str[0].toUpperCase();
 
-    for (i=1; i<str.length; i++){
+    for (let i = 1; i < name.length; i++) { 
         strNew = strNew + str[i];
     }
     return strNew;
 }
 document.forms.form_3.onsubmit = function(){
-    let name = document.getElementById('input-3').value;
-    document.getElementById("output-3").innerHTML = `Результат: ${capitalize(name)}`;
+    const name = document.getElementById('input-3').value;
+    const capitalizeName = capitalize(name);
+
+    document.getElementById("output-3").innerHTML = `Результат: ${capitalizeName}`;
     return false;
 }
 
 // 4. Створити функцію, яка вираховує суму, що залишається після оплати податку від зарабітньої плати. 
 // (Податок = 18% + 1.5% -> 19.5%). Приклад: 1000 -> 805
 
-function getSalary(salary, tax){
-    let taxAmount = salary * tax / 100;
-    let newSalary = salary - taxAmount;
+function getSalary(salary, tax) { 
+    const taxAmount = salary * tax / 100;
+    const newSalary = salary - taxAmount;
     return newSalary;
 }
 
-document.forms.form_4.onsubmit = function(){
-    let salary = Number(document.getElementById('input-4_1').value);
-    let tax = Number(document.getElementById('input-4_2').value);
+document.forms.form_4.onsubmit = function() {
+    const salary = Number(document.getElementById('input-4_1').value);
+    const tax = Number(document.getElementById('input-4_2').value);
+    const taxableSalary = getSalary(salary, tax);
 
-    document.getElementById("output-4").innerHTML = `Вам нараховано: ${getSalary(salary, tax)}грн`;
+    document.getElementById("output-4").innerHTML = `Вам нараховано: ${taxableSalary}грн`;
     return false;
 }
 
 // 5. Створити функцію, яка повертає випадкове ціле число в діапазоні від N до M. 
 // Приклад: getRandomNumber(1, 10) -> 5
 
-function getRandomNumber(numberStart, numberFinish, seed){
-    number1 = Math.min(numberStart, numberFinish);
-    number2 = Math.max(numberStart, numberFinish);
+function getRandomNumber(numberStart, numberFinish, seed) {
+    const number1 = Math.min(numberStart, numberFinish);
+    const number2 = Math.max(numberStart, numberFinish);
     let random = (seed * seed).toString().padStart(4, '0').slice(1,3);
-    seed2 = parseInt(random);
-    while (seed == seed2){
+    let seed2 = parseInt(random);
+    while (seed === seed2){
         seed2 = date.getSeconds();
         random = (seed2 * seed2).toString().padStart(4, '0').slice(1,3);
         seed2 = parseInt(random);
     }
     seed = seed2;
-    let div = number2 - number1 + 1;
-    let result = number1 + seed % div ;
+    const div = number2 - number1 + 1;
+    const result = number1 + seed % div ;
     return result;
 }
-document.forms.form_5.onsubmit = function(){
+document.forms.form_5.onsubmit = function() {
     let date = new Date();
     let seed = date.getSeconds();
-    let number1 = Number(document.getElementById('input-5_1').value);
-    let number2 = Number(document.getElementById('input-5_2').value);
+    const number1 = Number(document.getElementById('input-5_1').value);
+    const number2 = Number(document.getElementById('input-5_2').value);
+    const randomNumber = getRandomNumber(number1, number2, seed);
 
-    document.getElementById("output-5").innerHTML = `Ваше число: ${getRandomNumber(number1, number2, seed)}`;
+    document.getElementById("output-5").innerHTML = `Ваше число: ${randomNumber}`;
     return false;   
 }
-
-
 
 // 6. Створити функцію, яка рахує скільки разів певна буква повторюється в слові. 
 // Приклад: countLetter("а", "Асталавіста") -> 4
 
-function countLetter(letter, word){
-    word = word.toLowerCase();
-    letter = letter.toLowerCase();
+function getCountLetter(letter, word){
+    const wordLower = word.toLowerCase();
+    const letterLower = letter.toLowerCase();
     let amount = 0;
-    for(i=0; i<word.length; i++){
-        if (word[i]==letter){
+    for(let i=0; i<word.length; i++) {
+        if (wordLower[i]===letterLower) {
             amount += 1;
         }
     }
     return amount;
 }
-document.forms.form_6.onsubmit = function(){
-    
-    let word = document.getElementById('input-6_1').value;
-    let letter = document.getElementById('input-6_2').value;
+document.forms.form_6.onsubmit = function() {
+    const word = document.getElementById('input-6_1').value;
+    const letter = document.getElementById('input-6_2').value;
+    const countLetter = getCountLetter(letter, word);
 
-    document.getElementById("output-6").innerHTML = `Буква '${letter}' повторюється ${countLetter(letter, word)} разів`;
+    document.getElementById("output-6").innerHTML = `Буква '${letter}' повторюється ${countLetter} разів`;
     return false;   
 }
 
@@ -145,93 +150,88 @@ document.forms.form_6.onsubmit = function(){
 // Приклад: convertCurrency("100$") -> 2500 грн. або convertCurrency("2500UAH") -> 100$
 // Врахуйте, інші валюти не конвертуються, потрібно виводити помилку, і також регістр uah не має значення.
 
-function convertCurrency(str){
-    str = str.toUpperCase();
+function convertCurrency(input) {
+    const str = input.toUpperCase();
     let newStr = '';
     let output;
-    for (i=0; i<str.length; i++){
-
-        if (str[i]=='$'){
-            output = Number(newStr) * 25; 
-            output = output + ' UAH';
-        }
-        if (str[i]=='U'&& str[i+1]=="A" && str[i+2]=="H"){
-            output = Number(newStr) / 25; 
-            output = output + ' $';
-        }
-
+    for (let i=0; i<str.length; i++) {
+        if (Number(str[i]))
         newStr = newStr + str[i];
     }
-    if (output == undefined){
+    if (str.match(/\$/)) {
+        output = `${Number(newStr) * 25} UAH`; 
+    }
+    if (str.match('UAH')) {
+        output = `${Number(newStr) / 25} $`; 
+    }
+    if (output === undefined) {
         return "Вкажіть валюту '$' або 'UAH' " ;
     }
 
     return output;
 }
 document.forms.form_7.onsubmit = function(){
-    let input = document.getElementById('input-7').value;
+    const inputCurrency = document.getElementById('input-7').value;
 
-    document.getElementById("output-7").innerHTML = convertCurrency(input);
+    document.getElementById("output-7").innerHTML = convertCurrency(inputCurrency);
     return false;   
 }
 
 // 8. Створіть функцію генерації випадкового паролю (тільки числа), довжина встановлюється користувачем або по замовчуванню = 8 символам.
 // Приклад: getRandomPassword(4) -> 1875, getRandomPassword() -> 87240124
 
-function getRandomPassword(numbers){
+function getRandomPassword(numbers = 8) {
     let password = '';
 
-    if (numbers==""){
-        for (i=0; i<8; i++){
-            password = password + Math.trunc(Math.random()*10);
-        }
-    } else{
-        for (i=0; i<numbers; i++){
-            password = password + Math.trunc(Math.random()*10);
-        }
+    for (let i = 0; i < numbers; i++) {
+        password = password + Math.trunc(Math.random() * 10);
     }
     return password;
 }
-document.forms.form_8.onsubmit = function(){
+document.forms.form_8.onsubmit = function() {
     let numbers = document.getElementById('input-8').value;
+    if (numbers === "") {
+        numbers = undefined;
+    }
+    const password = getRandomPassword(numbers);
 
-    document.getElementById("output-8").innerHTML = `Ваш новий пароль: ${getRandomPassword(numbers)}`;
+    document.getElementById("output-8").innerHTML = `Ваш новий пароль: ${password}`;
     return false;   
 }
 
 // 9. Створіть функцію, яка видаляє всі букви з речення. 
 // Приклад: deleteLetters('a', "blablabla") -> "blblbl"
 
-function deleteLetters(letter, text){
+function deleteLetters(letter, text) {
     let output = '';
 
-    for(i=0; i<text.length; i++){
-        if (text[i]==letter.toUpperCase() || text[i]==letter.toLowerCase()){
+    for(let i=0; i<text.length; i++) {
+        if (text[i]===letter.toUpperCase() || text[i]===letter.toLowerCase()) {
             continue
-        }else {
+        } else {
             output = output + text[i];
         }
     }
     return output;
 }
 document.forms.form_9.onsubmit = function(){
-    let letter = document.getElementById('input-9_1').value;
-    let text = document.getElementById('input-9_2').value;
-
-    document.getElementById("output-9").innerHTML = `Нове речення без букви '${letter}': "${deleteLetters(letter, text)}"`;
+    const letter = document.getElementById('input-9_1').value;
+    const text = document.getElementById('input-9_2').value;
+    const newText = deleteLetters(letter, text);
+    document.getElementById("output-9").innerHTML = `Нове речення без букви '${letter}': "${newText}"`;
     return false;   
 }
 
 // 10. Створіть функцію, яка перевіряє, чи є слово паліндромом. 
 // Приклад: isPalyndrom("мадам") -> true, isPalyndrom("кокос") -> false, isPalyndrom("Я несу гусеня") -> true
 
-function isPalyndrom(word){
+function isPalyndrom(word) {
     let palyndrom;
     let x = word.length;
 
-    for (let i = 0; i < (word.length / 2); i++){
+    for (let i = 0; i < (word.length / 2); i++) {
         x = x-1;
-        if (word[i] == word[x]){
+        if (word[i] === word[x]) {
             palyndrom = true;
         } else {
             palyndrom = false;
@@ -241,7 +241,8 @@ function isPalyndrom(word){
     return palyndrom;
 }
 document.forms.form_10.onsubmit = function(){
-    let word = document.getElementById('input-10').value;
+    const word = document.getElementById('input-10').value;
+
     document.getElementById("output-10").innerHTML = isPalyndrom(word);
     return false;   
 }
@@ -249,10 +250,7 @@ document.forms.form_10.onsubmit = function(){
 // 11. Створіть функцію, яка видалить з речення букви, які зустрічаються більше 1 разу. 
 // Приклад: deleteDuplicateLetter("Бісквіт був дуже ніжним") -> "сктдеим"
 
-
-
-function deleteDuplicateLetter(textIn){
-    // let textIn = 'ааБбвгггддdk,fv';
+function deleteDuplicateLetter(textIn) {
     let textOut = [];
     let newStr = '';
     textIn = textIn.toLowerCase();
@@ -264,8 +262,8 @@ function deleteDuplicateLetter(textIn){
     }
     for (let i=0; i<length; i++){
     flag = false;
-      for ( let x=i+1; x<length; x++){
-        if (textOut[i] == textOut[x]){
+      for ( let x=i+1; x<length; x++) {
+        if (textOut[i] === textOut[x]) {
             textOut[x] = undefined;
             flag = true;
         }
@@ -282,7 +280,8 @@ function deleteDuplicateLetter(textIn){
     return newStr;
 }
 document.forms.form_11.onsubmit = function(){
-    let textIn = document.getElementById('input-11').value;
+    const textIn = document.getElementById('input-11').value;
+
     document.getElementById("output-11").innerHTML = deleteDuplicateLetter(textIn);
     return false;   
 }

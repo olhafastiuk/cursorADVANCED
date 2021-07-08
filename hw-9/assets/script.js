feb78a53f136740ec730ba6600b50899d2277b37
Olha Fastiuk <olhafastiuk@gmail.com>
2021-07-09 00:04:16 +0300

    HW#9: Reduce function generalBlocksInterval

--- hw-9/assets/script.js
+++ hw-9/assets/script.js
@@ -3,59 +3,52 @@ let amount;
 let column;
 let row;
 let stopInterval;
+
 function getData() {
     size = +document.getElementById("size").value;
-    amount = +document.getElementById("amount").value;
+    column = +document.getElementById("row").value;
     row = +document.getElementById("column").value;
-    column = amount / row;
+    amount = column * row;
 }
+
+const basic = "BASIC";
+const advanced = "ADVANCED";
 const mainBlock = document.getElementById("main-block");
 const mainBlock2 = document.getElementById("main-block2");
 const rowBlock = document.getElementsByClassName("row");
 const blocks = document.getElementsByClassName("block");
 
-function generateBlocks() {
-    if (!size || !amount || !row ) {
-        alert ('Введіть значення')
-        return
+function generateBlocks(mainBlock, basic) {
+    if (!size || !amount || !row) {
+        alert("Введіть значення");
+        return;
     }
     mainBlock.querySelectorAll(".row").forEach((el) => el.remove());
     mainBlock.querySelectorAll(".title").forEach((el) => el.remove());
-    mainBlock.insertAdjacentHTML("beforeend", `<h3 class = "title">BASIC</h3>`);
-    
+    mainBlock.insertAdjacentHTML( "beforeend", `<h3 class = "title">${basic}</h3>`);
+
     let block = 0;
     for (let i = 0; i < column; i++) {
         mainBlock.insertAdjacentHTML("beforeend", `<div class = "row"></div>`);
         for (let a = 0; a < row && block < amount; a++) {
-            rowBlock[i].insertAdjacentHTML("beforeend", `<div class = "block" ></div>`);
+            if (mainBlock === mainBlock2) {
+                let index = document.querySelectorAll(".row").length - 1;
+                rowBlock[index].insertAdjacentHTML("beforeend", `<div class = "block" ></div>`);
+            } else {
+                rowBlock[i].insertAdjacentHTML("beforeend", `<div class = "block" ></div>`);
+            }
             block += 1;
         }
+    }
+    if (mainBlock === mainBlock2) {
+        return block;
     }
     generateBlocksStyle(block, 0);
 }
 
 function generateBlocksInterval() {
-    if (!size || !amount || !row ) {
-        alert ('Введіть значення')
-        return
-    }
     clearTimeout(stopInterval);
-    mainBlock2.querySelectorAll(".row").forEach((el) => el.remove());
-    mainBlock2.querySelectorAll(".title").forEach((el) => el.remove());
-    mainBlock2.insertAdjacentHTML("beforeend", `<h3 class = "title">ADVANCED</h3>`);
-    
-    let block = 0;
-    for (let i = 0; i < column; i++) {
-        mainBlock2.insertAdjacentHTML("beforeend", `<div class = "row"></div>`);
-        for (let a = 0; a < row && block < amount; a++) {
-            let index = document.querySelectorAll(".row").length - 1;
-            rowBlock[index].insertAdjacentHTML(
-                "beforeend",
-                `<div class = "block" ></div>`
-            );
-            block += 1;
-        }
-    }
+    let block = generateBlocks(mainBlock2, advanced);
     run(block);
 }
 
@@ -75,4 +68,3 @@ function run(number) {
     generateBlocksStyle(number + x, x);
     stopInterval = setTimeout(run, 1000, number);
 }
-

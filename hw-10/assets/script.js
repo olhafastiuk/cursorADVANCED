88bba4fdc4d908a2b00c84a06c34de8e8a404420
Olha Fastiuk <olhafastiuk@gmail.com>
2021-07-11 15:29:39 +0300

    HW#10: Add styles

--- hw-10/assets/script.js
+++ hw-10/assets/script.js
@@ -4,7 +4,7 @@ document.onkeydown = function (e) {
     if (document.getElementById(e.code)) {
         remover();
         document.getElementById(e.code).classList.add("active");
-        document.getElementById(e.code).getElementsByTagName('audio')[0].play()
+        document.getElementById(e.code).getElementsByTagName("audio")[0].play();
     }
     console.log("a");
 };
@@ -12,7 +12,7 @@ for (let i = 0; i < btn.length; i++) {
     btn[i].addEventListener("click", function () {
         remover();
         btn[i].classList.add("active");
-        btn[i].getElementsByTagName('audio')[0].play()
+        btn[i].getElementsByTagName("audio")[0].play();
     });
 }
 

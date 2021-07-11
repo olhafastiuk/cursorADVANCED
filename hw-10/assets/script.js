7f10f623ca508f1780741615664a164998cbf489
Olha Fastiuk <olhafastiuk@gmail.com>
2021-07-11 15:59:52 +0300

    HW#10: Reset audio after new tap

--- hw-10/assets/script.js
+++ hw-10/assets/script.js
@@ -1,18 +1,20 @@
 let btn = document.getElementsByTagName("button");
+let play;
 
 document.onkeydown = function (e) {
     if (document.getElementById(e.code)) {
         remover();
         document.getElementById(e.code).classList.add("active");
-        document.getElementById(e.code).getElementsByTagName("audio")[0].play();
+        play = document.getElementById(e.code).getElementsByTagName("audio")[0];
+        play.play();
     }
-    console.log("a");
 };
 for (let i = 0; i < btn.length; i++) {
     btn[i].addEventListener("click", function () {
         remover();
         btn[i].classList.add("active");
-        btn[i].getElementsByTagName("audio")[0].play();
+        play = btn[i].getElementsByTagName("audio")[0];
+        play.play();
     });
 }
 
@@ -20,5 +22,7 @@ function remover() {
     let prev = document.getElementsByClassName("active");
     if (prev[0]) {
         prev[0].classList.remove("active");
+        play.pause();
+        play.currentTime = 0;
     }
 }

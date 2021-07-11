1de266dc0084b6c4da07e926e56b9b812b728b2b
Olha Fastiuk <olhafastiuk@gmail.com>
2021-07-11 14:59:55 +0300

    HW#10: Add button and sounds

---
+++ hw-10/assets/script.js
@@ -0,0 +1,24 @@
+let btn = document.getElementsByTagName("button");
+
+document.onkeydown = function (e) {
+    if (document.getElementById(e.code)) {
+        remover();
+        document.getElementById(e.code).classList.add("active");
+        document.getElementById(e.code).getElementsByTagName('audio')[0].play()
+    }
+    console.log("a");
+};
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", function () {
+        remover();
+        btn[i].classList.add("active");
+        btn[i].getElementsByTagName('audio')[0].play()
+    });
+}
+
+function remover() {
+    let prev = document.getElementsByClassName("active");
+    if (prev[0]) {
+        prev[0].classList.remove("active");
+    }
+}

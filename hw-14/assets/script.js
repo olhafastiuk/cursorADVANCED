b68b87af9bc8c3f57621666493b69ddbcb9dfaef
Olha Fastiuk <olhafastiuk@gmail.com>
2021-07-22 23:26:36 +0300

    HW #14: Add layout

--- hw-14/assets/script.js
+++ hw-14/assets/script.js
@@ -1,37 +1,39 @@
 const idGenerator = createIdGenerator();
 function* createIdGenerator() {
     let i = 1;
-    while(true){
+    while (true) {
         yield i++;
     }
 }
 
+document.getElementById("endless").addEventListener("click", () => {
+    document.querySelector(".res-1").innerHTML = idGenerator.next().value;
+});
 
 const fontGenerator = newFontGenerator(14);
-
 function* newFontGenerator(size) {
-    let res = yield size
-
-    while(true) {
+    let res = yield size;
+    while (true) {
         if (res === "up") {
             size = size + 2;
-            res = yield size
+            res = yield size;
         } else if (res === "down") {
             size = size - 2;
-            res = yield size
-        }
-        else res = yield size
+            res = yield size;
+        } else res = yield size;
     }
-    }
+}
 
-console.log(fontGenerator.next("up").value)
-console.log(fontGenerator.next("up").value)
-console.log(fontGenerator.next("up").value)
-console.log(fontGenerator.next().value)
-console.log(fontGenerator.next("down").value)
-console.log(fontGenerator.next("down").value)
-console.log(fontGenerator.next("down").value)
-console.log(fontGenerator.next().value)
+document.getElementById("btn-down").addEventListener("click", () => {
+    fontGenerator.next("down");
+    document
+        .querySelector(".font")
+        .setAttribute("style", `font-size: ${fontGenerator.next().value}px`);
+});
 
-
-
+document.getElementById("btn-up").addEventListener("click", () => {
+    fontGenerator.next("up");
+    document
+        .querySelector(".font")
+        .setAttribute("style", `font-size: ${fontGenerator.next().value}px`);
+});
